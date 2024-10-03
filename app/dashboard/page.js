b55d8b8d68b06b9be70f7f8373fa59da982244a4
Tushar7105 <tushar070105@gmail.com>
2024-10-03 17:22:22 +0530

    add authentication

---
+++ app/dashboard/page.js
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function Dashboard(){
+    return (
+        <div>Dashboard
+            <UserButton></UserButton>
+        </div>
+    )
+}
+export default Dashboard
